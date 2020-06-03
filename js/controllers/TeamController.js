cd77d7f4e37791dafe82d63748515a82742d7c56
mariacardoso233 <mariacardoso233@gmail.com>
2020-06-03 18:08:22 +0100

    js

--- js/controllers/TeamController.js
+++ js/controllers/TeamController.js
@@ -1,81 +1,15 @@
-import TeamModel from '../models/TeamModel'
+import TeamModel from '../models/TeamModel.js'
 
 export default class TeamController {
     constructor() {
         this.teamModel = new TeamModel()
     }
 
-    addTeam(nome, localizacao, descricao, foto, membros) {
-        if (!this.teamModel.getAll().some(team => team.name === name)) {
-            this.teamModel.create(
-                nome,
-                localizacao,
-                descricao,
-                foto,
-                membros
-            );
-        } else {
-            throw Error(`Equipa com o nome "${name}" já existe!`);
-        }
-    }
-
-    removeTeam(name) {
-        this.teamModel.remove(name)
-    }
-
-    setCurrentTeam(id) {
-        this.teamModel.setCurrentTeam(id)
-    }
-
-    getCurrentTeam() {
-        return this.teamModel.getCurrentTeam()
-    }
-
-    /*
-
-    removeBand(name) {
-        this.bandModel.remove(name)
-    }
-
-    setCurrentBand(id) {
-        this.bandModel.setCurrentBand(id)
-    }
-
-    getCurrentBand() {
-        return this.bandModel.getCurrentBand()
+    getTeams() {
+        const teams = this.teamModel.getAll()
+        return teams;
     }
 
 
-    getBands(filterName='', filterGenre='', isSorted=false) {
-
-        if (isSorted) {
-            this.bandModel.sort()
-        }
-
-        const bands = this.bandModel.getAll()
-        
-        if (filterName==='' && filterGenre==='') {
-            return bands
-        }
-
-        let filteredBands = []
-
-        for (const band of bands) {
-            let filterBandName = false, filterBandGenre = false
-
-            if((band.name.includes(filterName) && filterName!='') || filterName==='') {
-                filterBandName = true
-            }
-
-            if((band.genre===filterGenre && filterGenre!='') || filterGenre==='') {
-                filterBandGenre = true
-            }
-
-            // Alimentar filteredBands
-            if(filterBandName && filterBandGenre) {
-                filteredBands.push(band)
-            }
-        }
-
-        return filteredBands*/
+    /**/
 }
--- js/models/TeamModel.js
+++ js/models/TeamModel.js
@@ -1,6 +1,6 @@
 export default class TeamModel {
     constructor() {
-        this.teams = localStorage.teams ? JSON.parse(localStorage.teams) : [];
+        this.teams = localStorage.teans ? JSON.parse(localStorage.teams) : [];
     }
 
     getAll() {
@@ -8,35 +8,37 @@ export default class TeamModel {
     }
 
     create(nome, localizacao, descricao, foto, membros) {
-        const team = {
-            id: this.teams.length > 0 ? this.teams[this.teams.length - 1].id + 1 : 1,
-            nome: nome,
-            localizacao: localizacao,
-            descricao: descricao,
-            foto: foto,
-            membros: membros
-        }
-
-        this.teams.push(team);
-        this._persist();
-    }
-
-    edit(nome, localizacao, descricao, foto, membros) {
-        const teams = this.teams;
-
-        for (let i = 0; i < teams.length; i++) {
-            const team = teams[i];
-            if (i == id - 1) {
-                team.nome = nome;
-                team.localizacao = localizacao;
-                team.descricao = descricao;
-                team.foto = foto;
-                team.membros = membros;
+            const team = {
+                id: this.teams.length > 0 ? this.teams[this.teams.length - 1].id + 1 : 1,
+                nome: nome,
+                localizacao: localizacao,
+                descricao: descricao,
+                foto: foto,
+                membros: membros
             }
+
+            this.teams.push(team);
+            this._persist();
         }
-        localStorage.setItem('teams', JSON.stringify(teams));
-        location.reload();
-    }
+        /*
+
+        edit(nome, localizacao, descricao, foto, membros) {
+            const teams = this.teams;
+
+            for (let i = 0; i < teams.length; i++) {
+                const team = teams[i];
+                if (i == id - 1) {
+                    team.nome = nome;
+                    team.localizacao = localizacao;
+                    team.descricao = descricao;
+                    team.foto = foto;
+                    team.membros = membros;
+                }
+            }
+            localStorage.setItem('teams', JSON.stringify(teams));
+            location.reload();
+        }
+        */
 
     _persist() {
         localStorage.setItem('teams', JSON.stringify(this.teams));
