bd851af43b6ed3ae4bd54caf5a8d153949564990
mariacardoso233 <mariacardoso233@gmail.com>
2020-06-11 15:12:35 +0100

    dsc

---
+++ js/views/EditProfileView.js
@@ -0,0 +1,23 @@
+import UserController from '../controllers/UserController.js'
+
+export default class EditProfileView {
+
+    constructor() {
+        this.userController = new UserController();
+
+        //DOM References
+        this.userNome = document.querySelector('#txtNovoNome')
+        this.userLocalidade = document.querySelector('#sltLocalidade')
+        this.userSobrenome = document.querySelector('#txtNovoSobrenome')
+        this.userGenero = document.querySelector('#sltGenero')
+        this.userMail = document.querySelector('#txtNovoMail')
+        this.userNome = document.querySelector('#txtNovoNome')
+        this.userNome = document.querySelector('#txtNovoNome')
+        this.userNome = document.querySelector('#txtNovoNome')
+        this.userNome = document.querySelector('#txtNovoNome')
+        this.userNome = document.querySelector('#txtNovoNome')
+
+
+
+    }
+}
